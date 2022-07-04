87cd68294e6e8bf1a560331419c5b643d13c78a4
KrishnanRGithub <vinokrish001@gmail.com>
2022-07-04 05:49:10 +0530

    extracted build

---
+++ precache-manifest.c1b3c0b8fd53dfb8a22a31005a7cfd16.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "428b2f3ef200bade71f8bfd42f40aafa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33e477addd9a4d783d3c",
+    "url": "/static/css/main.3af25472.chunk.css"
+  },
+  {
+    "revision": "444668ecda67263f2cc8",
+    "url": "/static/js/2.eafda6ab.chunk.js"
+  },
+  {
+    "revision": "4336e3572b0698fb13dc6a49b9696dc0",
+    "url": "/static/js/2.eafda6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e477addd9a4d783d3c",
+    "url": "/static/js/main.38cb4819.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.38cb4819.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d0fffb2c85a824d4e45",
+    "url": "/static/js/runtime-main.ee952f88.js"
+  }
+]);
